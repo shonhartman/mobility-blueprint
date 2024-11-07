03efd4b317c78681852f77262a20b0db974b1377
Shadman Ahmed <shadifmg@gmail.com>
2024-11-07 14:20:30 -0600

    added a separator between sign and social sign ins

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -159,6 +159,17 @@ export default function SignIn() {
               </button>
             </div>
           </form>
+          {/* Separator */}
+          <div className="relative my-6">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
