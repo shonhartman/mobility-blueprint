725991e4d1b13922cd9f2c2fb386360ee2660b71
shonhartman <shaun@bighuman.com>
2024-11-22 15:19:34 -0500

    chore: fix viewport setting for deploy

---
+++ src/app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,5 @@
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  // ... any other viewport settings
+}
--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -12,7 +12,7 @@ import config from '../../../../sanity.config'
 
 export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
+export { metadata } from 'next-sanity/studio'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
