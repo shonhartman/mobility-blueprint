a9f1b0384d89c61cf004fbf66a041d5b12d649f8
shonhartman <shaun@bighuman.com>
2024-11-25 11:56:23 -0500

    chore: redirect home links to dashboard

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const HomeButton: React.FC = () => {
   return (
-    <Link href="/">
+    <Link href="/dashboard">
       <button className="absolute top-4 right-4 bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-600">
-        Home
+        Dashboard
       </button>
     </Link>
   )
